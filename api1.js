d320ab8f05bae0967851497814650d7748739c79
sleter <sleter97@gmail.com>
2018-03-08 13:57:19 +0100

    basic api2

--- api1.js
+++ api1.js
@@ -1,3 +1,9 @@
+var words = {
+  'good': 1,
+  'neutral': 0,
+  'bad': -1
+}
+
 console.log('server is starting');
 
 var express = require('express');
@@ -10,9 +16,43 @@ function listening(){
 
 app.use(express.static('website'));
 
-app.get('/search/:catchword', returnInfo);
+app.get('/add/:catchword/:score?', addInfo);
 
-function returnInfo(request, response){
+function addInfo(request, response){
   var data = request.params;
-  response.send('Your request: ' + data.catchword);
+  var word = data.catchword;
+  var score = Number(data.score);
+  if(!score){
+    response.send('Score is required');
+    return;
+  }
+  words[word] = score;
+  response.send('Your request: ' + data.catchword +' is in database');
+}
+
+app.get('/json', sendJson);
+
+function sendJson(request, response){
+  response.send(words);
+}
+
+app.get('/search/:catchword', searchInfo);
+
+function searchInfo(request, response){
+  var word = request.params.catchword;
+  var reply;
+  if(words[word]){
+    reply = {
+      status: "found",
+      word: word,
+      score: words[word]
+    }
+  }
+  else{
+    reply = {
+      status: "not found",
+      word: word
+    }
+  }
+  response.send(reply);
 }
